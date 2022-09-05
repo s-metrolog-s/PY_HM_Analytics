5dd21a70711e6a72bd90f362fd256de389131876
Sergey <metrolog_omsk@mail.ru>
2022-09-05 22:27:09 +0500

    Сумма значений последовательности по формуле

---
+++ Task_203.py
@@ -0,0 +1,13 @@
+# Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
+
+n = int(input('Введите число N: '))
+
+list_numbers = []
+sum = 0
+
+for i in range(1, n + 1):
+    result = round((1 + 1/i)**i, 2)
+    sum += result
+    list_numbers.append(result)
+
+print(f'Сумма последовательности {list_numbers} равна {sum}')
